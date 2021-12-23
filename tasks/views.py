177b9e3f0d6144b45506352524fee87523981329
Ada Meceffeuk <46904388+averroes96@users.noreply.github.com>
2021-12-23 19:19:06 +0100

    CRUD Operations

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
 def index(request):
     tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
     context = {
-        "tasks" : tasks
+        "tasks" : tasks,
+        "form" : form
     }
     return render(request, "tasks/list.html", context)
+
+def update(request, pk):
+
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {
+        "task" : task,
+        "form" : form
+    }
+    return render(request, "tasks/update_task.html", context)
+
+def delete(request, pk):
+
+    task = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        task.delete()
+        return redirect("index")
+        
+    return render(request, "tasks/confirm.html", {"task" : task})
